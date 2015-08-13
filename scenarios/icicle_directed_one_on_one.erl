8fa42279c7e2f0a0090c6a1412679d5f9c4290b5
Joseph Yiasemides <joseph.yiasemides@erlang-solutions.com>
2015-08-13 16:58:44 +0200

    Add a directed (one-way) relay-to-relay load scenario
    In this scenario there are a pair of TURN relays for each TURN
    client/peer pair. Clients send, and peers receive, through their
    respective relays. I.e. client ---> relay ---> relay ---> peer. The
    message in the scenario is very simple: a sequence of messages will
    test the system better.

---
+++ scenarios/icicle_directed_one_on_one.erl
@@ -0,0 +1,106 @@
+%% The scenario herein allocates a TURN relay to each client, then
+%% pairs them together: one sends data while the other receives data
+%% through both of their respective relays. See notes under start/1
+%% for details on the kind of network traffic to expect, i.e. when
+%% monitoring this test.
+
+-module(icicle_directed_one_on_one).
+
+-export([init/0]).
+-export([start/1]).
+
+-define(SOCK_TABLE, sockets).
+
+-define(USERNAME, <<"alice">>).
+-define(PASSWORD, <<"ali">>).
+-define(TEST_CREDENTIALS, {?USERNAME, ?PASSWORD}).
+
+-define(TURN_PORT, 34780).
+-define(REMOTE_INSTANCE, {54,172,65,222}).
+-define(TEST_SERVER_SOCK, {?REMOTE_INSTANCE, ?TURN_PORT}).
+
+-define(DATA, <<"Hello, world!">>).
+
+
+init() ->
+    ets:new(?SOCK_TABLE, [public, named_table, set]),
+    ok.
+
+start(Id) when Id >= 0 ->
+    %% NETWORK TRAFFIC: All procedures but the send/3 and
+    %% get_message_buffer/1 make requests to a TURN server, expecting
+    %% a response, so that's 2 datagrams for each. The send/3 will
+    %% simply send 1 STUN formatted "indication", with NO response to
+    %% be received. As for get_message_buffer/1, this is a call, but
+    %% only to the generic server process (that is, the client).
+    {ok, Client} = icicle_client_server:start_link(?TEST_CREDENTIALS,
+        ?TEST_SERVER_SOCK),
+    failure_response = icicle_client_server:request_relay(Client),
+    success_response = icicle_client_server:request_relay(Client),
+    Sock = icicle_client_server:get_relay_address(Client),
+
+    %% === Below mocks out of band commumication =====
+    ok = put_client_in_table(Id, Sock),
+    %% SLEEP FOR ALL TO PUT
+    timer:sleep(48 * 1000),
+    {ok, Peer} = get_peer_from_table(Id),
+    %% === Above mocks out of band communication =====
+
+    permission_success = icicle_client_server:set_permission(Client, Peer),
+    case parity(Id) of
+	odd ->
+	    icicle_client_server:send(Client, ?DATA, Peer);
+	even ->
+	    timer:sleep(32 * 1000),
+	    case icicle_client_server:get_message_buffer(Client) of
+		[{Peer, ?DATA}|_More] ->
+		    ok;
+		[] ->
+		    error(got_nothing)
+	    end
+    end,
+    lager:info("Id ~p done.", [Id]),
+    ok = icicle_client_server:stop(Client).
+
+put_client_in_table(Id, Sock) ->
+    %% Make sure the mapping between client and their socket is only
+    %% done once (to safeguard against anything strange happening).
+    true  = ets:insert_new(?SOCK_TABLE, {Id, Sock}),
+    lager:info("Id ~p put in table", [Id]),
+    ok.
+
+get_peer_from_table(Id) ->
+    %% Why do we delete rows from the table in the second "case"?
+    %% Because when `amoc_local:do/3' is done it's called again after
+    %% some defualt time, enumerating the Id's as before, but the ETS
+    %% table and continues to exist and holds these values. That would
+    %% mess up the safeguard we put in place when inserting into the
+    %% table.
+    case parity(Id) of
+	even ->
+	    Peer = partner_one_down(Id);
+	odd ->
+	    Peer = partner_one_up(Id)
+    end,
+    case ets:lookup(?SOCK_TABLE, Peer) of
+	[{Peer, Sock}] ->
+	    true = ets:delete(?SOCK_TABLE, Peer),
+	    {ok, Sock};
+	[] ->
+	    lager:info("Id ~p not in table", [Peer]),
+	    {error, no_peer_in_table}
+    end.
+
+parity(Num) when is_integer(Num) ->
+    case Num rem 2 =:= 0 of
+	true ->
+	    even;
+	false ->
+	    odd
+    end.
+	    
+partner_one_down(Id) when is_integer(Id), Id > 0 ->
+    Id - 1.
+
+partner_one_up(Id) when is_integer(Id), Id > 0 ->
+    Id + 1.
