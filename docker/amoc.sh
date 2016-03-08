aa39bd07c7ab4cc444c80ab29960de1163318a1c
Rafal Studnicki <rafal.studnicki@erlang-solutions.com>
2016-03-08 09:59:40 +0100

    Add vm.args to docker image

--- docker/amoc.sh
+++ docker/amoc.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-exec /sbin/setuser amoc /home/amoc/amoc/bin/amoc console -noshell -noinput +Bd \
-    -kernel inet_dist_listen_min 5000 inet_dist_listen_max 5030
+exec /sbin/setuser amoc /home/amoc/amoc/bin/amoc console -noshell -noinput +Bd
