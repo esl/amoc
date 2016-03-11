07e89cbedaf78469c529936077d6541a2d9e6d5c
Rafal Studnicki <rafal.studnicki@erlang-solutions.com>
2016-03-11 14:47:24 +0100

    Add amoc hosts variable to the sys.config template

--- docker/run.sh
+++ docker/run.sh
@@ -2,8 +2,10 @@
 
 set -x
 
+SYSCONFIG="/home/amoc/amoc/releases/0.9/sys.config"
 sed -e "s/AMOC_GRAPHITE_HOST/${AMOC_GRAPHITE_HOST}/" /sys.config.template > \
-    /home/amoc/amoc/releases/0.9/sys.config
-sed -i -e "s/AMOC_PREFIX/${AMOC_PREFIX}/" /home/amoc/amoc/releases/0.9/sys.config
+    ${SYSCONFIG}
+sed -i -e "s/AMOC_PREFIX/${AMOC_PREFIX}/" ${SYSCONFIG}
+sed -i -e "s/AMOC_HOSTS/${AMOC_HOSTS}/" ${SYSCONFIG}
 
 /sbin/my_init
