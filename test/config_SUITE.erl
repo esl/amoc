e52fc0776395458e6b58f0be2f8c291bb0fca09c
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2019-08-29 09:22:13 +0200

    add positive test cases for parsing scenario params

--- test/config_SUITE.erl
+++ test/config_SUITE.erl
@@ -12,7 +12,10 @@ all() ->
      config_appenv_test,
      config_none_test,
      config_osappenv_test,
-     config_osenv_dynamic_test].
+     config_osenv_dynamic_test,
+     parse_scenario_settings_returns_map_if_all_params_are_valid,
+     parse_scenario_settings_uses_default_value_if_a_param_is_not_set,
+     parse_scenario_settings_shadows_default_value].
 
 config_osenv_test(_Config) ->
     given_osenv_set([{"AMOC_interarrival", "100"},
@@ -63,3 +66,42 @@ config_osenv_dynamic_test(_) ->
     os:putenv("AMOC_foo", "baz"),
     %% then
     ?assertEqual(baz, amoc_config:get(foo)).
+
+parse_scenario_settings_returns_map_if_all_params_are_valid(_) ->
+    given_amoc_started(),
+    ScenarioParams = correct_scenario_params(),
+    given_scenario_parameters_set(ScenarioParams),
+    Result = amoc_config:parse_scenario_settings(ScenarioParams),
+    ?assertMatch({ok, SettingsMap} when is_map(SettingsMap), Result).
+
+parse_scenario_settings_uses_default_value_if_a_param_is_not_set(_) ->
+    given_amoc_started(),
+    ScenarioParams = correct_scenario_params(),
+    given_scenario_parameters_set(ScenarioParams),
+    SettingsWithNotSetParam = [{not_set_param, 'NOT_SET_SCENARIO_PARAM',  1234, none} | ScenarioParams],
+    {ok, Settings} = amoc_config:parse_scenario_settings(SettingsWithNotSetParam),
+    ?assertMatch({ok, 1234}, amoc_config:get_scenario_parameter(not_set_param, Settings)).
+
+parse_scenario_settings_shadows_default_value(_) ->
+    given_amoc_started(),
+    ScenarioParams = correct_scenario_params(),
+    given_scenario_parameters_set(ScenarioParams),
+    [{Name, Var, OriginalValue, _} | Rest] = ScenarioParams,
+    NewRandomValue = base64:encode(crypto:strong_rand_bytes(5)),
+    SettingsWithChangedDefault = [{Name, Var, NewRandomValue, none} | Rest],
+    {ok, Settings} = amoc_config:parse_scenario_settings(SettingsWithChangedDefault),
+    ?assertMatch({ok, OriginalValue}, amoc_config:get_scenario_parameter(Name, Settings)).
+
+
+
+correct_scenario_params() ->
+    [
+     {iq_timeout,         'IQ_TIMEOUT',                  10000, positive_integer},
+     {coordinator_delay,  'COORDINATOR_DELAY',               0, nonnegative_integer},
+     {activation_policy,  'ACTIVATION_POLICY',       all_nodes, [all_nodes, n_nodes]},
+     {mim_host,           'MIM_HOST',          <<"localhost">>, bitstring}
+    ].
+
+given_scenario_parameters_set(ScenarioParams) ->
+    Params = [{Name, Value} || {_, Name, Value, _} <- ScenarioParams],
+    application:set_env([{amoc, Params}]).
