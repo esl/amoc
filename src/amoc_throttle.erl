95c898aaf288853398d7cd1310b693f013b29f9c
Gustaw Lippa <gustaw.lippa@erlang-solutions.com>
2019-09-17 12:13:29 +0200

    Add diagram concerning amoc in distributed env

--- src/amoc_throttle.erl
+++ src/amoc_throttle.erl
@@ -52,6 +52,42 @@ change_rate_gradually(Name, LowRate, HighRate, RateInterval, StepInterval, NoOfS
 
 -spec run(name(), fun(()-> any())) -> ok | {error, any()}.
 run(Name, Fn) ->
+
+    %% Diagram showing function execution flow in distributed env.
+    %% generated using https://sequencediagram.org/ (remove "%%"):
+    %%
+    %% 	  title Amoc distributed
+    %%    participantgroup  **Slave node**
+    %%        participant User
+    %%        participant Async runner
+    %%    end
+    %%    participantgroup **Master node**
+    %%        participant Throttle process
+    %%    end
+    %%    box left of User: inc req rate
+    %%
+    %%    User -> *Async runner : Fun
+    %%
+    %%    User -> Throttle process : {schedule, Async runner PID}
+    %%    box right of Throttle process : inc req rate
+    %%
+    %%        ==throtlling delay==
+    %%
+    %%    Throttle process -> Async runner:{scheduled, Throttle process PID, Interval}
+    %%
+    %%    box left of Async runner : inc exec rate
+    %%    abox over Async runner : Fun()
+    %%    activate Async runner
+    %%    box right of Throttle process : inc exec rate
+    %%    deactivate Async runner
+    %%    Async runner -> Throttle process : increase n
+    %%    space 1
+    %%
+    %%    destroy Async runner
+    %%
+    %% for the local execution, req/exec rates are increased only by throttle process.
+
+
     amoc_throttle_controller:run(Name, Fn).
 
 -spec send(name(), pid(), any()) -> ok | {error, any()}.
