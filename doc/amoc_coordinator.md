934dad8aabecf19b9552b08dcdd94c90b44f3134
Denys Gonchar <denys.gonchar@erlang-solutions.com>
2019-11-04 14:21:07 +0100

    moved interfaces from amoc_local to amoc module & removed amoc_local module

--- doc/amoc_coordinator.md
+++ doc/amoc_coordinator.md
@@ -127,7 +127,7 @@ To run it:
 $ make rel
 $ _build/default/rel/amoc/bin/amoc console
 
-1> amoc_local:do(example, 1, 5).
+1> amoc:do(example, 1, 5).
 ```
 
 Filtered, formated and explained output:
@@ -135,7 +135,7 @@ Filtered, formated and explained output:
 ```erlang
 User = 1  % First user is started
 
-ok  % result of calling amoc_local:do/3
+ok  % result of calling amoc:do/3
 
 User = 2 % First user is started
 
--- src/amoc.erl
+++ src/amoc.erl
@@ -4,6 +4,10 @@
 %%==============================================================================
 -module(amoc).
 
+-export([do/3,
+         add/1,
+         remove/2]).
+
 -export_type([
               scenario/0,
               do_opts/0,
@@ -18,3 +22,20 @@
 
 -type remove_opt() :: {force, boolean()}.
 -type remove_opts() :: [remove_opt()].
+
+%% ------------------------------------------------------------------
+%% API for local scenario execution, use amoc_dist module to run
+%% scenarios in a distributed environment
+%% ------------------------------------------------------------------
+-spec do(amoc:scenario(), amoc_scenario:user_id(), amoc_scenario:user_id()) ->
+    ok | {error, term()}.
+do(Scenario, Start, End) ->
+    amoc_controller:do(Scenario, Start, End).
+
+-spec add(non_neg_integer()) -> ok.
+add(Count) ->
+    amoc_controller:add(Count).
+
+-spec remove(non_neg_integer(), amoc:remove_opts()) -> ok.
+remove(Count, Opts) ->
+    amoc_controller:remove(Count, Opts).
