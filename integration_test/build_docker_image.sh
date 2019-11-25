d2504247bf9eb4898fb44aa727c7cc5f61d9df6c
Denys Gonchar <denys.gonchar@erlang-solutions.com>
2019-11-25 12:05:58 +0100

    fixing integration tests

--- integration_test/build_docker_image.sh
+++ integration_test/build_docker_image.sh
@@ -2,12 +2,12 @@
 
 # Get current repo version
 VERSION=$(git rev-parse --short HEAD)
-ERLANG_VSN=${OTP_VSN:-21.3.8.7-1}
-echo "ERLANG/OTP ${ERLANG_VSN}"
+OTP_VSN="${TRAVIS_OTP_RELEASE:-21.3.8.7}-1"
+echo "ERLANG/OTP ${OTP_VSN}"
 
 docker build \
 	-f Dockerfile \
 	-t amoc:latest \
 	--build-arg vsn=${VERSION} \
-	--build-arg otp_vsn=${ERLANG_VSN} \
+	--build-arg otp_vsn=${OTP_VSN} \
 	.
