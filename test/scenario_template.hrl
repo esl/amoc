8a752760d17b77eb9847f41f4f6f751d1a8f4b09
DenysGonchar <denys.gonchar@erlang-solutions.com>
2020-08-03 14:35:32 +0200

    tests for scenario info rest api

--- test/scenario_template.hrl
+++ test/scenario_template.hrl
@@ -1,6 +1,13 @@
 -define(DUMMY_SCENARIO_MODULE(Name), <<"
+%%==============================================================================
+%% @doc
+%%   some edoc
+%% @end
+%%==============================================================================
 -module(",(atom_to_binary(Name,utf8))/binary,").
 -behaviour(amoc_scenario).
+
+-required_variable(#{name => some_parameter, description => \"some parameter\"}).
 
 -export([start/1]).
 -export([init/0]).
