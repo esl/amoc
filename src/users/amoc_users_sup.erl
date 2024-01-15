c9428623894bcca3b1d37a4fac82e92cf8224912
Nelson Vides <nelson.vides@erlang-solutions.com>
2024-01-15 10:29:50 +0100

    Explain and add specs to amoc_users_sup

--- src/users/amoc_users_sup.erl
+++ src/users/amoc_users_sup.erl
@@ -1,5 +1,13 @@
 %% @private
 %% @copyright 2023 Erlang Solutions Ltd.
+%% @doc Top supervisor of the pooled users supervisor.
+%%
+%% It spawns a pool of workers as big as online schedulers. When starting a new user, as the user is
+%% identified by ID, a worker will be chosen for this user based on its ID
+%% (see get_sup_for_user_id/1).
+%%
+%% The currently running number of users is stored in an atomic that all workers update and the
+%% controller can read.
 -module(amoc_users_sup).
 
 -behaviour(supervisor).
@@ -41,6 +49,19 @@ init(no_args) ->
     {ok, {Strategy, Specs}}.
 
 %% API
+-spec init_storage() -> any().
+init_storage() ->
+    UserSups = supervisor:which_children(?MODULE),
+    UserSupPids = [ Pid || {_, Pid, _, _} <- UserSups ],
+    UserSupPidsTuple = erlang:list_to_tuple(UserSupPids),
+    Atomic = atomics:new(1, [{signed, false}]),
+    atomics:put(Atomic, 1, 0),
+    Storage = #storage{user_count = Atomic, sups = UserSupPidsTuple},
+    persistent_term:put(?MODULE, Storage).
+
+-spec clean_storage() -> any().
+clean_storage() ->
+    persistent_term:erase(?MODULE).
 
 -spec count_no_of_users() -> non_neg_integer().
 count_no_of_users() ->
@@ -67,6 +88,9 @@ start_child(Scenario, Id, ScenarioState) ->
     Sup = get_sup_for_user_id(Id),
     gen_server:cast(Sup, {start_child, Scenario, Id, ScenarioState}).
 
+%% Group all children based on ID to their respective worker supervisor and cast a request with each
+%% group at once. This way we reduce the number of casts to each worker to always one, instead of
+%% depending on the number of users.
 -spec start_children(amoc:scenario(), [amoc_scenario:user_id()], any()) -> ok.
 start_children(Scenario, UserIds, ScenarioState) ->
     KeyFun = fun(UserId) ->
@@ -78,13 +102,15 @@ start_children(Scenario, UserIds, ScenarioState) ->
               end,
     maps:foreach(CastFun, Assignments).
 
+%% Assign a count of children each worker needs to remove
+%% in order to load-balance the request among all workers.
 -spec stop_children(non_neg_integer(), boolean()) -> non_neg_integer().
 stop_children(Count, Force) ->
     TotalCount = count_no_of_users(),
-    CountRemove = min(Count, TotalCount),
     #storage{sups = Sups} = persistent_term:get(?MODULE),
     NumOfSupervisors = tuple_size(Sups),
     ShuffledSupervisors = shuffle_supervisors(tuple_to_list(Sups)),
+    CountRemove = min(Count, TotalCount),
     Assignments = assign_counts_to_supervisors(ShuffledSupervisors, NumOfSupervisors, CountRemove),
     [ gen_server:cast(Sup, {stop_children, Int, Force}) || {Sup, Int} <- Assignments ],
     CountRemove.
@@ -94,22 +120,8 @@ terminate_all_children() ->
     #storage{sups = Sups} = persistent_term:get(?MODULE),
     [ gen_server:cast(Sup, terminate_all_children) || Sup <- tuple_to_list(Sups) ].
 
-
-%%% Helpers
--spec init_storage() -> any().
-init_storage() ->
-    UserSups = supervisor:which_children(?MODULE),
-    UserSupPids = [ Pid || {_, Pid, _, _} <- UserSups ],
-    UserSupPidsTuple = erlang:list_to_tuple(UserSupPids),
-    Atomic = atomics:new(1, [{signed, false}]),
-    atomics:put(Atomic, 1, 0),
-    Storage = #storage{user_count = Atomic, sups = UserSupPidsTuple},
-    persistent_term:put(?MODULE, Storage).
-
--spec clean_storage() -> any().
-clean_storage() ->
-    persistent_term:erase(?MODULE).
-
+%% Helpers
+-spec get_sup_for_user_id(amoc_scenario:user_id()) -> pid().
 get_sup_for_user_id(Id) ->
     #storage{sups = Supervisors} = persistent_term:get(?MODULE),
     Index = Id rem tuple_size(Supervisors) + 1,
@@ -117,6 +129,7 @@ get_sup_for_user_id(Id) ->
 
 %% Create a list with random indexes with supervisors that will then be sorted
 %% Uses mwc59 for the most performant (though statistically unsound) erlang RNG
+-spec shuffle_supervisors([pid()]) -> [pid()].
 shuffle_supervisors(Supervisors) ->
     UniqueInt = erlang:unique_integer([positive]),
     F1 = fun(Sup, Int) ->
@@ -124,12 +137,16 @@ shuffle_supervisors(Supervisors) ->
                 {{Rand, Sup}, Rand}
         end,
     {Indexed, _} = lists:mapfoldl(F1, UniqueInt, Supervisors),
-    lists:sort(Indexed).
+    SortedByIndex = lists:sort(Indexed),
+    {_Indexes, ShuffledSupervisors} = lists:unzip(SortedByIndex),
+    ShuffledSupervisors.
 
+-spec assign_counts_to_supervisors([pid()], non_neg_integer(), non_neg_integer()) ->
+    [{pid(), non_neg_integer()}].
 assign_counts_to_supervisors(ShuffledList, NumOfSupervisors, CountRemove) ->
     SlotPerSup = CountRemove div NumOfSupervisors,
     Remainder = CountRemove rem NumOfSupervisors,
-    F2 = fun({_Index, Sup}, Rem) ->
+    F2 = fun(Sup, Rem) ->
                  {{Sup, SlotPerSup + Rem}, min(0, Rem - 1)}
          end,
     {Assignments, _} = lists:mapfoldl(F2, Remainder, ShuffledList),
